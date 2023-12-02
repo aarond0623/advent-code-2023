2bfa5826ee0337c3291a5a31a8d2d1d27414f7eb
Aaron Daughtry <aarondaughtry@gmail.com>
2023-12-02 13:22:42 -0500

    Separated part 1 into its own function

--- day-01/main.go
+++ day-01/main.go
@@ -8,6 +8,20 @@ import (
 	"strconv"
 )
 
+func part1(line string) int {
+	// Returns an integer found by finding the first and last digits in a string
+	// Match first digit
+	re_digit1 := regexp.MustCompile(`^\D*(\d)`)
+	// Match last digit
+	re_digit2 := regexp.MustCompile(`(\d)\D*$`)
+	// Get matches
+	digit1 := re_digit1.FindStringSubmatch(line)[1]
+	digit2 := re_digit2.FindStringSubmatch(line)[1]
+	// Add to sum
+	value, _ := strconv.Atoi(digit1 + digit2)
+	return value
+}
+
 func main() {
 	/* PART 1 */
 	// Initialize our sum to 0
@@ -15,16 +29,7 @@ func main() {
 	// Read input from stdin line by line
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		// Match first digit
-		re_digit1 := regexp.MustCompile(`^\D*(\d)`)
-		// Match last digit
-		re_digit2 := regexp.MustCompile(`(\d)\D*$`)
-		// Get matches
-		digit1 := re_digit1.FindStringSubmatch(scanner.Text())[1]
-		digit2 := re_digit2.FindStringSubmatch(scanner.Text())[1]
-		// Add to sum
-		value, _ := strconv.Atoi(digit1 + digit2)
-		sum += value
+		sum += part1(scanner.Text())
 	}
 
 	fmt.Println(sum)
