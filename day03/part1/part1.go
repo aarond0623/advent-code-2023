e4fff247422d6baeb87f165d9ba6ecd75493dd8d
Aaron Daughtry <aarondaughtry@gmail.com>
2023-12-03 09:52:04 -0500

    Day 3: Gear Ratios, implemented testing functions

--- day03/part1/part1.go
+++ day03/part1/part1.go
@@ -1,5 +1,9 @@
 package part1
 
+func IsPartNumber(lines []string, row int, slice []int) bool {
+	return false
+}
+
 func Solve(lines []string) int {
 	return 0
 }
--- day03/part1/part1_test.go
+++ day03/part1/part1_test.go
@@ -5,6 +5,49 @@ import (
 	"testing"
 )
 
+func TestIsPartNumber(t *testing.T) {
+	t.Parallel()
+
+	lines := []string{
+		"467..114..",
+		"...*......",
+		"..35..633.",
+		"......#...",
+		"617*......",
+		".....+.58.",
+		"..592.....",
+		"......755.",
+		"...$.*....",
+		".664.598..",
+	}
+
+	type testCase struct {
+		row   int
+		slice []int
+		want  bool
+	}
+
+	testCases := []testCase{
+		{row: 0, slice: []int{0, 3}, want: true},
+		{row: 0, slice: []int{5, 8}, want: false},
+		{row: 2, slice: []int{2, 4}, want: true},
+		{row: 2, slice: []int{6, 9}, want: true},
+		{row: 4, slice: []int{0, 3}, want: true},
+		{row: 5, slice: []int{7, 9}, want: false},
+		{row: 6, slice: []int{2, 5}, want: true},
+		{row: 7, slice: []int{6, 9}, want: true},
+		{row: 9, slice: []int{1, 4}, want: true},
+		{row: 9, slice: []int{5, 8}, want: true},
+	}
+
+	for _, tc := range testCases {
+		got := part1.IsPartNumber(lines, tc.row, tc.slice)
+		if tc.want != got {
+			t.Errorf("IsPartNumber(%d, {%d, %d}): want %t, got %t", tc.row, tc.slice[0], tc.slice[1], tc.want, got)
+		}
+	}
+}
+
 func TestSolve(t *testing.T) {
 	t.Parallel()
 
@@ -28,6 +71,6 @@ func TestSolve(t *testing.T) {
 	got := part1.Solve(testCase)
 
 	if want != got {
-		t.Errorf("want %d, got %d", want, got)
+		t.Errorf("Solve: want %d, got %d", want, got)
 	}
 }
